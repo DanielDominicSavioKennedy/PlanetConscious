1583b3acda7f1422590728bb5600b34379b13d19
SiddharthKoyugura <koyugurasiddhardha@gmail.com>
2023-03-04 12:17:10 +0530

    auth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
 import datetime
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
+from functools import wraps
 
 
 app = Flask(__name__)
@@ -11,8 +13,7 @@ app.config["SECRET_KEY"] = "WebsiteMadeByWebLaunch2022"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
-
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = "Users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text, unique=True, nullable=False)
@@ -21,13 +22,23 @@ class User(db.Model):
 
 db.create_all()
 
+# Login Manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 activites = []
 
 @app.route("/home")
+@login_required
 def home():
-    return render_template("index.html")
+    return render_template("index.html", current_user=current_user)
 
 @app.route("/table", methods=["GET", "POST"])
+@login_required
 def table():
     if request.method == "POST":
         status = request.form.get("status")
@@ -52,15 +63,22 @@ def table():
             })
         return redirect("table")
     print(activites)
-    return render_template("tables-general.html", activites=activites)
+    return render_template("tables-general.html", current_user=current_user, activites=activites)
 
 @app.route("/chart")
+@login_required
 def chart():
-    return render_template("charts.html")
+    return render_template("charts.html", current_user=current_user)
 
 @app.route("/profile")
+@login_required
 def profile():
-    return render_template("profile.html")
+    return render_template("profile.html", current_user=current_user)
+
+@app.route("/blog")
+@login_required
+def blog():
+    return render_template("blogs.html", current_user=current_user)
 
 @app.route("/", methods=["GET", "POST"])
 def register():
@@ -79,6 +97,7 @@ def register():
         )
         db.session.add(new_user)
         db.session.commit()
+        login_user(new_user)
         return redirect(url_for('home'))
 
     return render_template("register.html")
@@ -91,6 +110,7 @@ def login():
         if user:
             password = request.form.get("password")
             if user.password == password:
+                login_user(user)
                 return redirect(url_for("home"))
  
             flash("Invalid password")
@@ -100,6 +120,12 @@ def login():
         return redirect(url_for("login"))
     
     return render_template("login.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(host="localhost", port=3000, debug=True)
